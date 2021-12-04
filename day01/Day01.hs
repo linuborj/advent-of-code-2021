8f6f390c6060e96d90bdf42984cd553264b47901
Linus Börjesson <linus@borjesson.io>
2021-12-04 10:08:45 +0100

    Day 1 - Part 1

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -3,29 +3,23 @@ module Main where
 import Paths_day01
 
 
-data Change = Decrease | Unchanged | Increase
-  deriving (Show, Eq)
-
-change x y
-  | x < y  = Increase
-  | x == y = Unchanged
-  | x > y  = Decrease
-
-changes :: [Int] -> [Change]
-changes xs = zipWith change xs (tail xs)
-
 readInput :: IO [Int]
 readInput = do
   fileName <- getDataFileName "input"
   content <- readFile fileName
   return . map read . lines $ content
 
+
+comparisons :: [Int] -> [Ordering]
+comparisons xs = zipWith compare (tail xs) xs
+
 part1 :: IO ()
 part1 = do
   input <- readInput
-  let increases = length . filter (== Increase) . changes $ input
+  let increases = length . filter (== GT) . comparisons $ input
   putStr "Part1: "
   print increases
+
 
 main :: IO ()
 main = do
