c3e4b165d7ac409348765c008aaf97fcf2b6430c
Linus Börjesson <linus@borjesson.io>
2021-12-04 10:01:08 +0100

    Day 1 - Part 1

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -1,8 +1,33 @@
 module Main where
 
-factorial :: Integral a => a -> a
-factorial n = product [1..n]
+import Paths_day01
 
-main :: IO()
-main = print . sum . map (read . (:[])) . show $ factorial 100
+
+data Change = Decrease | Unchanged | Increase
+  deriving (Show, Eq)
+
+change x y
+  | x < y  = Increase
+  | x == y = Unchanged
+  | x > y  = Decrease
+
+changes :: [Int] -> [Change]
+changes xs = zipWith change xs (tail xs)
+
+readInput :: IO [Int]
+readInput = do
+  fileName <- getDataFileName "input"
+  content <- readFile fileName
+  return . map read . lines $ content
+
+part1 :: IO ()
+part1 = do
+  input <- readInput
+  let increases = length . filter (== Increase) . changes $ input
+  putStr "Part1: "
+  print increases
+
+main :: IO ()
+main = do
+  part1 -- prints `Part1: 1553`
 
