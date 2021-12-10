ca7b052b7cfb55aea5221c58bc7d098454abaa9a
Linus Börjesson <linus@borjesson.io>
2021-12-10 18:58:45 +0100

    Day 1 - Part 2

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -9,7 +9,6 @@ readInput = do
   content <- readFile fileName
   return . map read . lines $ content
 
-
 comparisons :: [Int] -> [Ordering]
 comparisons xs = zipWith compare (tail xs) xs
 
@@ -20,8 +19,20 @@ part1 = do
   putStr "Part1: "
   print increases
 
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow n xs
+  | length xs < n = []
+  | otherwise     = take n xs : slidingWindow n (tail xs)
+
+part2 :: IO ()
+part2 = do
+  input <- readInput
+  let increases = length . filter (== GT) . comparisons . map sum . slidingWindow 3 $ input
+  putStr "Part2: "
+  print increases
 
 main :: IO ()
 main = do
   part1 -- prints `Part1: 1553`
+  part2 -- prints `Part2: 1597`
 
