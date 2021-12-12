52080400f7b0ed4f4eab3064f105d3dcd92e11fd
Linus Börjesson <linus@borjesson.io>
2021-12-12 21:09:12 +0100

    Day 6 - Part 2

--- day06/Day06.hs
+++ day06/Day06.hs
@@ -3,43 +3,56 @@ module Main where
 import Paths_day06
 import Text.ParserCombinators.ReadP
 import qualified Data.Char as Char
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
 
+
+newtype State = State { unState :: IntMap Integer }
+  deriving (Show, Read, Eq)
+
+state :: [Int] -> State
+state = State . foldr (\age -> IntMap.insertWith (+) age 1) IntMap.empty
 
 parseInt :: ReadP Int
 parseInt = do
   digits <- many1 $ satisfy Char.isDigit
   return $ read digits
 
-parser :: String -> [Int]
-parser = fst . last . readP_to_S (parseInt `sepBy1` char ',')
+parser :: String -> State
+parser = state . fst . last . readP_to_S (parseInt `sepBy1` char ',')
 
-readInput :: IO [Int]
+readInput :: IO State
 readInput = do
   fileName <- getDataFileName "input"
   content <- readFile fileName
   return . parser $ content
 
-evolve' :: Int -> [Int]
-evolve' x
-  | x < 0     = error "Illegal state"
-  | x == 0    = [8, 6]
-  | otherwise = [x-1]
+evolve :: State -> State
+evolve = State . IntMap.foldlWithKey evolve' IntMap.empty . unState
+  where
+    add (age, amount) sea = IntMap.insertWith (+) age amount sea
+    evolve' sea age amount
+      | age < 0  = error "Illegal state"
+      | age == 0 = (8, amount) `add` ((6, amount) `add` sea)
+      | otherwise = (age - 1, amount) `add` sea
 
-evolve :: [Int] -> [Int]
-evolve = concatMap evolve'
+total :: State -> Integer
+total = IntMap.foldl (+) 0 . unState
 
 part1 :: IO ()
 part1 = do
-  state0 <- readInput
+  input <- readInput
   putStr "Part1: "
-  print . length $ iterate evolve state0 !! 80
+  print . total $ iterate evolve input !! 80
 
 part2 :: IO ()
 part2 = do
+  input <- readInput
   putStr "Part2: "
+  print . total $ iterate evolve input !! 256
 
 main :: IO ()
 main = do
   part1 -- prints 'Part1: 377263'
-  part2 -- prints 'Part2: '
+  part2 -- prints 'Part2: 1695929023803'
 
