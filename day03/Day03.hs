c94abfb4240382379fd9c475a2aac1e67c37c276
Linus Börjesson <linus@borjesson.io>
2021-12-12 12:45:35 +0100

    Day 3 - Part 2

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -3,34 +3,90 @@ module Main where
 import Paths_day03
 import Text.ParserCombinators.ReadP
 import qualified Data.Char as Char
+import qualified Data.List as List
 
-parseDigits :: Read a => ReadP [a]
-parseDigits = do
-  digits <- many1 $ satisfy Char.isDigit
-  return $ map (read . (:[])) digits
 
-parser :: Read a => String -> [[a]]
-parser = fst . last . readP_to_S (parseDigits `sepBy1` char '\n')
+data Bit = Zero | One
+  deriving (Show, Eq)
 
-readInput :: IO [[Int]]
+decimal :: [Bit] -> Int
+decimal = sum . zipWith (*) (map (2^) [0..]) . map f . reverse
+  where
+    f Zero = 0
+    f One  = 1
+
+
+data Statistics = Statistics
+  { zeroes :: Int
+  , ones :: Int
+  } deriving (Show, Read, Eq)
+
+statistics0 :: Statistics
+statistics0 = Statistics 0 0
+
+mostCommonBit :: Statistics -> Bit
+mostCommonBit (Statistics zeroes ones)
+  | zeroes > ones = Zero
+  | zeroes < ones = One
+  | otherwise     = One
+
+leastCommonBit :: Statistics -> Bit
+leastCommonBit (Statistics zeroes ones)
+  | zeroes < ones = Zero
+  | zeroes > ones = One
+  | otherwise     = Zero
+
+updateStatistics :: Statistics -> Bit -> Statistics
+updateStatistics (Statistics zeroes ones) Zero = Statistics (zeroes + 1) ones
+updateStatistics (Statistics zeroes ones) One  = Statistics zeroes (ones + 1)
+
+
+parseBits :: ReadP [Bit]
+parseBits = do
+  bits <- many1 $ satisfy (`elem` "01") 
+  return $ map (\c -> if c == '1' then One else Zero) bits
+
+parser :: String -> [[Bit]]
+parser = fst . last . readP_to_S (parseBits `sepBy1` char '\n')
+
+readInput :: IO [[Bit]]
 readInput = do
   fileName <- getDataFileName "input"
   content <- readFile fileName
   return . parser $ content
 
-decimal :: [Bool] -> Int
-decimal = sum . zipWith (*) (map (2^) [0..]) . reverse . map (\x -> if x then 1 else 0)
+
+statistics :: [[Bit]] -> [Statistics]
+statistics = foldl (zipWith updateStatistics) (repeat statistics0)
+
 
 part1 :: IO ()
 part1 = do
   rows <- readInput
-  let sums = foldl1 (zipWith (+)) rows
-      gamma = map (< length rows `div` 2) sums
-      epsilon = map not gamma
+  let gammaRate   = decimal . map mostCommonBit  . statistics $ rows
+      epsilonRate = decimal . map leastCommonBit . statistics $ rows
   putStr "Part1: "
-  print $ decimal gamma * decimal epsilon
+  print $ gammaRate * epsilonRate
+
+
+matchCriteria :: (Statistics -> Bit) -> [[Bit]] -> [Bit]
+matchCriteria _        [x] = x
+matchCriteria criteria xs  = bit : matchCriteria criteria xs'
+  where
+    bit = criteria . head . statistics $ xs
+    xs' = map tail . filter ((== bit) . head) $ xs
+
+part2 :: IO ()
+part2 = do
+  rows <- readInput
+  let oxygenRating = decimal $ matchCriteria mostCommonBit rows
+      cO2Rating    = decimal $ matchCriteria leastCommonBit rows
+  putStr "Part2: "
+  print $ oxygenRating * cO2Rating
+
 
 main :: IO ()
-main =
+main = do
   part1 -- prints 'Part1: 2954600'
+  part2 -- prints 'Part2: 1662846'
 
