45da2ac99ced19c16401319b81bfa172425c4895
Linus Börjesson <linus@borjesson.io>
2021-12-30 18:25:27 +0100

    Day 10 - Part 2

--- day10/Day10.hs
+++ day10/Day10.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Paths_day10
 import qualified Data.Maybe as Maybe
+import qualified Data.List as List
 
 
 opens :: Char -> Bool
@@ -14,19 +15,19 @@ invert '}' = '{'
 invert ']' = '['
 invert c   = error $ "'" ++ [c] ++ "' is not a closing bracket"
 
-score :: Char -> Integer
-score '>' = 25137
-score ')' = 3
-score '}' = 1197
-score ']' = 57
-score c   = error $ "'" ++ [c] ++ "' is not a closing bracket"
+syntaxErrorScore :: Char -> Integer
+syntaxErrorScore '>' = 25137
+syntaxErrorScore ')' = 3
+syntaxErrorScore '}' = 1197
+syntaxErrorScore ']' = 57
+syntaxErrorScore c   = error $ "'" ++ [c] ++ "' is not a closing bracket"
 
 firstIllegalCharacter :: String -> Maybe Char
 firstIllegalCharacter = illegal ""
   where
     illegal "" "" = Nothing
     illegal "" (x:xs)
-      | opens x   = illegal [invert x] xs
+      | opens x   = illegal [x] xs
       | otherwise = Just x
     illegal _ "" = Nothing
     illegal (c:cs) (x:xs)
@@ -44,15 +45,43 @@ part1 :: IO ()
 part1 = do
   input <- readInput
   putStr "Part1: "
-  print . sum . map score . Maybe.mapMaybe firstIllegalCharacter $ input
+  print . sum . map syntaxErrorScore . Maybe.mapMaybe firstIllegalCharacter $ input
+
+autoCompletionScore :: String -> Integer
+autoCompletionScore = foldl (\total c -> total * 5 + score c) 0
+  where
+    score '<' = 4
+    score '(' = 1
+    score '{' = 3
+    score '[' = 2
+    score c   = error $ "'" ++ [c] ++ "' is not an opening bracket"
+
+incomplete :: String -> Maybe String
+incomplete = incomplete' ""
+  where
+    incomplete' "" "" = Nothing
+    incomplete' "" (x:xs)
+      | opens x = incomplete' [x] xs
+      | otherwise = Nothing
+    incomplete' cs "" = Just cs
+    incomplete' (c:cs) (x:xs)
+      | opens x       = incomplete' (x:c:cs) xs
+      | invert x == c = incomplete' cs xs
+      | otherwise     = Nothing
+
+middle :: Ord a => [a] -> a 
+middle xs = xs' !! (length xs `div` 2)
+  where
+    xs' = List.sort xs
 
 part2 :: IO ()
 part2 = do
   input <- readInput
   putStr "Part2: "
+  print . middle . map autoCompletionScore . Maybe.mapMaybe incomplete $ input
 
 main :: IO ()
 main = do
   part1 -- prints 'Part1: 370407'
-  part2 -- prints 'Part2: '
+  part2 -- prints 'Part2: 3249889609'
 
